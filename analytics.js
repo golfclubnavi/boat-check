4f9cd11c26f40aa45db405be55cad1ecaf24ae6c
golfclubnavi <matuoka152@gmail.com>
2026-09-15 02:58:38 +0900

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+
+  var measurementId = 'G-T5XWPN6Z2N';
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag('js', new Date());
+  window.gtag('config', measurementId, {
+    anonymize_ip: true
+  });
+
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=' + encodeURIComponent(measurementId);
+  document.head.appendChild(script);
+})();
