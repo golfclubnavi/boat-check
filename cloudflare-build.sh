628acc078ad7ef39e7d7474964007e4116cec533
golfclubnavi <matuoka152@gmail.com>
2026-09-14 20:57:19 +0900

    Add files via upload

---
+++ cloudflare-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eu
+
+rm -rf cloudflare-dist
+mkdir -p cloudflare-dist/data
+
+find . -maxdepth 1 -type f \( \
+  -name '*.html' -o \
+  -name '*.css' -o \
+  -name 'robots.txt' -o \
+  -name 'sitemap.xml' \
+\) -exec cp '{}' cloudflare-dist/ \;
+
+find data -maxdepth 1 -type f -name '*.json' ! -name 'today.json' \
+  -exec cp '{}' cloudflare-dist/data/ \;
